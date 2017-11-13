8cdc6d1f883f6a84a7e49d327adc09f7d8e1e891
Vemulapalli <v.pramodkrishna@gmail.com>
2017-11-13 09:48:00 -0600

    added react router and redux directories

---
+++ src/components/items_index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ItemsIndex extends Component {
+  render() {
+    return (
+      <div>
+        Items Index
+      </div>
+    );
+  }
+}
+
+export default ItemsIndex;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import reducers from './reducers';
+import PostsIndex from './components/items_index';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={PostsIndex} />
+      </div>
+    </BrowserRouter>
+  </Provider>
+  , document.getElementById('root'));
+
 registerServiceWorker();
