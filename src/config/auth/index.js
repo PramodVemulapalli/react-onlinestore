b300816d4af7c06eeafdd9884f0569e906f08530
Vemulapalli <v.pramodkrishna@gmail.com>
2017-11-13 10:57:08 -0600

    .gitignore is now working

---
+++ src/config/auth/index.js
@@ -0,0 +1,2 @@
+export * from './firebase_info_real';
+export * from './facebook_info_real';
--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter, Route } from 'react-router-dom';
-
+import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import PostsIndex from './components/items_index';
+
+import { firebaseConfig } from './config/auth';
+
 
 import registerServiceWorker from './registerServiceWorker';
 
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
+const createStoreWithMiddleware = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={createStoreWithMiddleware}>
     <BrowserRouter>
       <div>
         <Route path="/" component={PostsIndex} />
